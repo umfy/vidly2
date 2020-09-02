9358e3c996dd70f0ac0cbd93e8bd17cc3a1a0e78
umfy <39531680+umfy@users.noreply.github.com>
2020-09-02 13:51:06 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ require('./startup/prod')(app)
 //const p = Promise.reject(new Error('Something failed miserably!'))
 //p.then(() => console.log('Done'))
 
-const port = process.env.Port || 3000
+const port = process.env.PORT || 3000
 const server = app.listen(port, (req, res) =>
    logger.info(`Listening on port 3000...`)
 )
